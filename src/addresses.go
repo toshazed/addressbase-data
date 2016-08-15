6ca14a6f5b0166df89ce2ea0cdefb1c2a2732227
psd <psd@whatfettle.com>
2016-08-15 19:29:52 +0100

    Clarified parenting issues

--- src/addresses.go
+++ src/addresses.go
@@ -76,32 +76,32 @@ const (
 )
 
 type Address struct {
-	entry_date        string
-	address           string
-	parent_address    string
-	street            string
-	name              string
-	name_cy           string
-	primary_name      string
-	primary_name_cy   string
-	point             string
-	street_custodian  string
-	end_date          string
-	has_lpi           bool
-	property_type     string
-	postcode          string
+	entry_date       string
+	address          string
+	parent_address   string
+	street           string
+	name             string
+	name_cy          string
+	primary_name     string
+	primary_name_cy  string
+	point            string
+	street_custodian string
+	end_date         string
+	has_lpi          bool
+	property_type    string
+	postcode         string
 }
 
 const sep = "\t"
 
-var fixed_header bool = false
+var issue_header bool = false
 
-func fixed(uprn string, level string, field string, value string, text string) {
-	if !fixed_header {
-		fmt.Fprintln(fdFixes, strings.Join([]string{"uprn", "level", "field", "value", "text"}, sep))
-		fixed_header = true
+func issue(street_custodian string, uprn string, level string, field string, value string, text string) {
+	if !issue_header {
+		fmt.Fprintln(fdFixes, strings.Join([]string{"street-custodian", "uprn", "level", "field", "value", "text"}, sep))
+		issue_header = true
 	}
-	fmt.Fprintln(fdFixes, strings.Join([]string{uprn, level, field, value, text}, sep))
+	fmt.Fprintln(fdFixes, strings.Join([]string{street_custodian, uprn, level, field, value, text}, sep))
 }
 
 func Name(sn string, ss string, en string, es string, t string) string {
@@ -200,7 +200,7 @@ func AddLPI(row []string) {
 		address.name_cy = name
 		address.primary_name_cy = primary_name
 	} else {
-		fixed(address.address, "error", "language", row[LPI_LANGUAGE], "unknown")
+		issue(address.street_custodian, address.address, "error", "language", row[LPI_LANGUAGE], "unknown")
 	}
 
 	address.end_date = row[LPI_END_DATE]
@@ -210,10 +210,10 @@ func AddLPI(row []string) {
 	records[address.address] = key
 }
 
-func PrimaryAddress(address string, parent_address string, primary_name string, depth int) string {
+func PrimaryAddress(street_custodian string, address string, parent_address string, primary_name string, depth int) string {
 
 	if depth > 32 {
-		fixed(address, "error", "parent_address", parent_address, "*probably looping*")
+		issue(street_custodian, address, "error", "parent_address", parent_address, "*probably looping*")
 		return ""
 	}
 
@@ -222,13 +222,13 @@ func PrimaryAddress(address string, parent_address string, primary_name string, 
 	}
 
 	if parent_address == "" {
-		fixed(address, "error", "parent_address", parent_address, "missing")
 		return ""
 	}
 
 	parent_key, ok := records[parent_address]
 	if !ok {
-		fixed(address, "error", "parent_address", parent_address, "unknown")
+		// it's possible that the parent isn't in the same grid square
+		issue(street_custodian, address, "error", "parent_address", parent_address, "unknown")
 		return ""
 	}
 	parent := entries[parent_key]
@@ -237,11 +237,12 @@ func PrimaryAddress(address string, parent_address string, primary_name string, 
 		return parent_address
 	}
 
-	primary_address := PrimaryAddress(address, parent.parent_address, primary_name, depth+1)
+	primary_address := PrimaryAddress(street_custodian, address, parent.parent_address, primary_name, depth+1)
 
 	if primary_address == "" {
-		fixed(address, "error", "primary_name", primary_name, "unable to find primary_name, parent.name ["+parent.name+"]")
+		issue(street_custodian, address, "note", "primary_name", primary_name, "doesn't match parent.name ["+parent.name+"]")
 	}
+
 	return ""
 }
 
@@ -253,24 +254,31 @@ func PrintAddress(address Address) {
 	}
 
 	if address.street == "" {
-		fixed(address.address, "ignored", "entry", address.entry_date, "missing street")
+		issue(address.street_custodian, address.address, "skipped", "entry", address.entry_date, "missing street")
 	}
 
 	if address.name == "STREET RECORD" {
-		fixed(address.address, "ignored", "record", "STREET RECORD", "dummy")
+		issue(address.street_custodian, address.address, "skipped", "record", "STREET RECORD", "dummy")
 		return
 	}
 	if address.street_custodian == "7655" {
 		// skip OS injected OWPA (object without postal address) records
-		fixed(address.address, "skipped", "record", address.street_custodian, "OWPA")
+		issue(address.street_custodian, address.address, "skipped", "record", address.street_custodian, "OWPA")
 		return
 	}
 
-	// search for the address which matches the PAO
-	primary_address := PrimaryAddress(address.address, address.parent_address, address.primary_name, 0)
+	// search for the primary address which matches the PAO
+	primary_address := ""
 
-	if primary_address == address.parent_address {
-		primary_address = ""
+	if address.primary_name != "" {
+		if address.parent_address == "" {
+			issue(address.street_custodian, address.address, "error", "parent_address", address.primary_name, "missing parent_address")
+		} else {
+			primary_address = PrimaryAddress(address.street_custodian, address.address, address.parent_address, address.primary_name, 0)
+			if primary_address == "" {
+				issue(address.street_custodian, address.address, "error", "primary_name", address.primary_name, "not found")
+			}
+		}
 	}
 
 	fmt.Fprintln(fdPostcodes, strings.Join([]string{
